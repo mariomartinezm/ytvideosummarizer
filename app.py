ddc6016aea12c27a1d101dfe8c2d0851db17b2d3
Mario M. M. <mariomartinezmolina@live.com>
2025-01-10 13:37:37 -0600

    Update app.py
    Add a document similarity search to find the relevants chunks in the
    video transcript.

--- app.py
+++ app.py
@@ -74,8 +74,8 @@ if url:
     st.write(response["answer"])
 
     # With a streamlit expander
-#     with st.expander("Document Similarity Search"):
-        # # Find the relevant chunks
-        # for i, doc in enumerate(response["context"]):
-            # st.write(doc.page_content)
-            # st.write("-------------------------------")
+    with st.expander("Document Similarity Search"):
+        # Find the relevant chunks
+        for i, doc in enumerate(response["context"]):
+            st.write(doc.page_content)
+            st.write("-------------------------------")
